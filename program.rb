642dae71f89b62efad4c0d0e1c736cf5087f1e00
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-09-18 15:39:30 -0700

    guess generator, initalize

--- program.rb
+++ program.rb
@@ -1,18 +1,116 @@
-def get_random_word(dictionary)
-  dictionary_array = []
-  dictionary.each {|row| dictionary_array.push(row)}
-
-  hangman_password = dictionary_array.sample.strip
-
-  until hangman_password.length >= 5 && hangman_password.length <= 12
-    hangman_password = dictionary_array.sample.strip
+class Game
+  def initialize
+    @guess_array = []
   end 
 
-  hangman_password
+  def get_random_word(dictionary)
+    dictionary_array = dictionary.map(&:strip)
+
+    hangman_password = ""
+    until hangman_password.length.between?(5, 12)
+      hangman_password = dictionary_array.sample.strip
+    end 
+
+    hangman_password
+  end 
+
+  def ask_for_guess
+    guess = ""
+
+    until guess =~ /^[a-z]+/  
+      puts "please enter a letter you have not already guessed"
+      guess = gets.chomp
+    end 
+
+    if @guess_array.include?(guess)
+      puts "You have already guessed this letter. Please enter another letter"
+      ask_for_guess
+    else
+      guess
+    end
+  end
+
+  def display_game_progress
+    hangman_array = stages = ['
+    ------
+    |    |
+    |
+    |
+    |
+    |
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |
+    |
+    |
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |    |
+    |    |
+    |
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |    |
+    |    |
+    |   /
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |    |
+    |    |
+    |   / \
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |  --|
+    |    |
+    |   / \
+    |
+------------
+', '
+    ------
+    |    |
+    |    O
+    |  --|--
+    |    |
+    |   / \
+    |
+------------
+']
+  end 
+
+  def game_turn
+    dictionary = File.readlines('lib/google-10000-english-no-swears.txt')
+    get_random_word(dictionary)
+
+    guess = ask_for_guess
+
+  end 
 end 
 
+
+
 #make an array of dictionary
-dictionary = File.readlines('lib/google-10000-english-no-swears.txt')
+
 
 #creates the hangman password
-get_random_word(dictionary)
+
+
